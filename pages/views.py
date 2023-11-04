edcce8ee963886bb8fcdacef0b980dc75c9ac66e
Miguelangel Avila <miguelangel.a1305@gmail.com>
2023-11-04 10:44:52 -0400

    come heces

---
+++ pages/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here idiot.
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+    
